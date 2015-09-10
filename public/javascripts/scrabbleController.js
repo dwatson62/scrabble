d4a5b566b3830764458b37dd7361baef3f6d65cf
Daryl Watson <darylwtsn@gmail.com>
2015-09-10 18:24:56 +0100

    Creates bag of letters

--- public/javascripts/scrabbleController.js
+++ public/javascripts/scrabbleController.js
@@ -1,6 +1,29 @@
 app.controller('ScrabbleController', ['$http', function($http) {
 
   var self = this;
+  self.player1Letters = []
+
+  self.createBag = function() {
+    var letters = _.keys(letterValues)
+    var bag = []
+    for (var letter in letters) {
+      for (i = 0; i < letterValues[letters[letter]].tiles; i ++) {
+        bag.push(letters[letter])
+      }
+    }
+    self.bag = _.shuffle(bag);
+    self.distributeLetters();
+  };
+
+  self.distributeLetters = function() {
+    self.player1Letters = []
+    var number = 7 - self.player1Letters.length
+    for (i = 0; i < number; i ++) {
+      var x = Math.floor((Math.random() * self.bag.length) + 1);
+      var letter = self.bag.splice(x, 1).join()
+      self.player1Letters.push(letter);
+    }
+  };
 
   self.giveWord = function() {
     var url = 'http://api.wordnik.com:80/v4/word.json/';
@@ -22,7 +45,8 @@ app.controller('ScrabbleController', ['$http', function($http) {
     var letters = self.input.split('');
     self.points = [];
     for (var x in letters) {
-      self.points.push(letterValues[letters[x]]);
+      var points = letterValues[letters[x]].points
+      self.points.push(points);
     }
   };
 
